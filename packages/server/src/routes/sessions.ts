84d36a77501ba7f9da310051c0f62f81cf419cd0
felipe <felipe.garciar@upb.edu.co>
2026-09-03 14:35:04 -0500

    04: server, shared and database packages fix2

--- packages/server/src/routes/sessions.ts
+++ packages/server/src/routes/sessions.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 // import { HTTPException } from "hono/http-exception";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
-import { db } from "@numencode/database";
+import { db } from "@numencode/database/client";
 import { Role, Mode, MessageStatus } from "@numencode/database/enums";
 import { findSupportedChatModel } from "@numencode/shared";
 
